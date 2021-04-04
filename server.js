2994d760c53703724d7694a56cb42356d996ea6b
aarush-paul <76874133+aarush-paul@users.noreply.github.com>
2021-04-04 22:41:30 +0530

    UPDATE

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-server.listen(1000);
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-app.get('/', (req, res) => {
-    res.redirect(`/index.html`);
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    console.log('Request for ' + req.url + ' by method ' + req.method);
+
+    if (req.method == 'GET') {
+        var fileUrl;
+        if (req.url == '/') fileUrl = '/index.html';
+        else fileUrl = req.url;
+    }
+    else {
+        filePath = path.resolve('./public/404.html');
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        fs.createReadStream(filePath).pipe(res);
+    }
+});
+
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
